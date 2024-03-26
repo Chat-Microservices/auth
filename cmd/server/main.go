b816dd4d28e0575bd5293f084e1aaecda056b6dc
Semyon <semhowork@gmail.com>
2024-03-26 08:45:15 +0300

    added api layout

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,18 +4,14 @@ import (
 	"context"
 	"flag"
 	"github.com/jackc/pgx/v4/pgxpool"
+	authAPI "github.com/semho/chat-microservices/auth/internal/api/auth"
 	"github.com/semho/chat-microservices/auth/internal/config"
 	"github.com/semho/chat-microservices/auth/internal/config/env"
-	"github.com/semho/chat-microservices/auth/internal/converter"
 	"github.com/semho/chat-microservices/auth/internal/repository/auth"
-	"github.com/semho/chat-microservices/auth/internal/service"
 	authService "github.com/semho/chat-microservices/auth/internal/service/auth"
 	desc "github.com/semho/chat-microservices/auth/pkg/auth_v1"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
 	"net"
 )
@@ -24,70 +20,6 @@ var configPath string
 
 func init() {
 	flag.StringVar(&configPath, "config-path", ".env", "path to config file")
-}
-
-type server struct {
-	desc.UnimplementedAuthV1Server
-	authService service.AuthService
-}
-
-func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.UserResponse, error) {
-	log.Printf("User id: %d", req.GetId())
-	userObj, err := s.authService.Get(ctx, req.GetId())
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("get user: %v", userObj)
-
-	return converter.ToAuthFromService(userObj), nil
-}
-
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	if req.GetDetail() == nil || req.GetPassword() == nil {
-		return nil, status.Error(codes.InvalidArgument, "Invalid request: Detail and Password must be provided")
-	}
-
-	if req.GetPassword().Password != req.GetPassword().PasswordConfirm {
-		return nil, status.Error(codes.InvalidArgument, "Password and Password Confirm do not match")
-	}
-
-	log.Printf("User name: %v", req.GetDetail().Name)
-	id, err := s.authService.Create(ctx, converter.ToAuthDetailFromDesc(req.GetDetail()), req.GetPassword().Password)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("inserted user with id: %d", id)
-
-	return &desc.CreateResponse{
-		Id: id,
-	}, nil
-}
-
-func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
-	if req.GetId() == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Invalid request: Id must be provided")
-	}
-	err := s.authService.Update(ctx, converter.ToAuthUpdateUserFromDesc(req))
-	if err != nil {
-		return nil, err
-	}
-
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	if req.GetId() == 0 {
-		return nil, status.Error(codes.InvalidArgument, "Invalid request: Id must be provided")
-	}
-
-	err := s.authService.Delete(ctx, req.GetId())
-	if err != nil {
-		return nil, err
-	}
-
-	return &emptypb.Empty{}, nil
 }
 
 func main() {
@@ -125,7 +57,7 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterAuthV1Server(s, &server{authService: authServ})
+	desc.RegisterAuthV1Server(s, authAPI.NewImplementation(authServ))
 
 	log.Printf("server listening at: %v", lis.Addr())
 
