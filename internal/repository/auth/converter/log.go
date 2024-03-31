6a76fe3c66cda7c3285492316fec0036d351fc29
Semyon <semhowork@gmail.com>
2024-03-31 15:10:46 +0300

    fix

--- internal/repository/auth/converter/log.go
+++ internal/repository/auth/converter/log.go
@@ -9,10 +9,12 @@ func ToLogFromRepo(logs []modelRepo.Log) []*model.Log {
 	convertedLogs := make([]*model.Log, len(logs))
 	for i, log := range logs {
 		convertedLogs[i] = &model.Log{
-			ID:       log.ID,
-			Action:   log.Action,
-			EntityId: log.EntityId,
-			Query:    log.Query,
+			ID:        log.ID,
+			Action:    log.Action,
+			EntityId:  log.EntityId,
+			Query:     log.Query,
+			CreatedAt: log.CreatedAt,
+			UpdatedAt: log.UpdatedAt,
 		}
 	}
 	return convertedLogs
